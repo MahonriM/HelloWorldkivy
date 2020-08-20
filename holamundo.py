7f7e9bc9a0e6d2436cb938cfe137bbb555d9b21c
MahonriM <cripto3000z@gmail.com>
2020-08-20 02:22:00 -0500

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,9 @@
+import kivy
+#kivy.require('1.10.0')
+from kivy.app import App
+from kivy.uix.label import Label
+class MiAplicacion(App):
+    def build(self):
+        return Label(text='Hola Mundo')
+if __name__ =="__main__":
+    MiAplicacion().run()
